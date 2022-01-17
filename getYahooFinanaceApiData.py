4fcf6134b2d632169cbfaa6d07b15f29c4df843e
Sharath Sahadevan <sharath.sahadevan@mendix.com>
2022-01-17 17:54:09 -0600

    Added functions and script
    Functions to parse the data. Script run.sh added.

--- getYahooFinanaceApiData.py
+++ getYahooFinanaceApiData.py
@@ -6,30 +6,62 @@ API_KEY= config('API_KEY')
 
 url = "https://yfapi.net/v6/finance/quote"
 
-#querystring = {"symbols":"AAPL,BTC-USD,EURUSD=X"}
-querystring = {"symbols":"AAPL"}
+# Function to get the data from the Yahoo API's
+# result has the json data
+def getResults(ticker):
+  #querystring = {"symbols":"AAPL,BTC-USD,EURUSD=X"}
+  #querystring = {"symbols":"AAPL"}
+  querystring = {"symbols": ticker }
 
-headers = {
+  headers = {
     'x-api-key': API_KEY
     }
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+  response = requests.request("GET", url, headers=headers, params=querystring)
 
-print(response.text)
+  print(response.text)
 
-apiData=json.loads(response.text)
+  apiData=json.loads(response.text)
 
-print (apiData)
+  print (apiData)
 
-keys=apiData.keys()
+  keys=apiData.keys()
 
-print(keys)
+  print(keys)
 
-result=apiData["quoteResponse"]["result"][0]
-resultKeys=result.keys()
+  result=apiData["quoteResponse"]["result"][0]
+  resultKeys=result.keys()
 
-print (result)
-print (resultKeys)
+  print (result)
+  print (resultKeys)
 
-for x,y in result.items():
-  print (x, y)
+  for x,y in result.items():
+    print (x, y)
+
+  print ("Market Price for " + ticker )
+  print (result["regularMarketPrice"])
+
+  return result
+
+# Get the value for the given key
+def getValueFromResult( result , key):
+    value=result[key]
+    return value
+
+def getRecommendation( result ):
+    recommendation="None"
+    forwardPe = getValueFromResult(myResult, "forwardPE")
+
+    if ( forwardPe < 20 ) :
+        recommendation="Buy"
+
+    return recommendation
+
+
+myTicker='AAPL'
+myResult=getResults(myTicker)
+
+print( getValueFromResult(myResult, "fiftyTwoWeekHighChangePercent") )
+print( getValueFromResult(myResult, "forwardPE") )
+
+print( getRecommendation(myResult) )
